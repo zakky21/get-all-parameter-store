7f11338c85216e78e7bf9945b8e7abf01dd28195
zakky21 <masanori.okazaki21@gmail.com>
2018-11-16 18:17:49 +0900

    initial commit

---
+++ getparameters.js
@@ -0,0 +1,48 @@
+const AWS = require('aws-sdk')
+const argv = require('argv')
+argv.option([
+  {
+    name: 'region',
+    short: 'r',
+    type: 'string'
+  },
+  {
+    name: 'accessKeyId',
+    short: 'a',
+    type: 'string'
+  },
+  {
+    name: 'secretAccessKey',
+    short: 's',
+    type: 'string'
+  }
+])
+
+const env = argv.run().options
+if (!env.accessKeyId) env.accessKeyId = process.env.AWS_ACCESS_KEY_ID
+if (!env.secretAccessKey) env.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+if (!env.region) env.region = process.env.AWS_DEFAULT_REGION
+
+const ssm = new AWS.SSM(env)
+
+async function call(token) {
+  const param = {
+    Path: '/',
+    Recursive: true
+  }
+  if (token) param.NextToken = token
+
+  ssm.getParametersByPath(param, (err, data) => {
+    if (err) console.log('error-occurd', err)
+    else {
+      for (let vv of data.Parameters) {
+        console.log(vv.Name, vv.Value)
+      }
+      if (data.NextToken) {
+        call(data.NextToken)
+      }
+    }
+  })
+}
+
+call()
